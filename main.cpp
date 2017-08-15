5472cd4b1f6e1144d490a64e7a4a8f95a114827e
isa sand <isasandh@gmail.com>
2017-08-15 14:45:40 +0200

    search function ok for now

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ using namespace std;
 int Menu();
 int RandomNumOrChar();
 string IDGenerator();
-int Search(vector <Employee*> listOfEmployees, int index);
+int Search(vector <Employee*> listOfEmployees, int index, int& option);
 int ProfileMenu();
 void ModifyProfile(vector <Employee*> listOfEmployees, int index, vector <int> indexPosition);
 
@@ -19,7 +19,7 @@ int main() {
 	string ID;
 	vector <Employee*>testEmployees;
 	int hardCodedIndex = 4;
-	int chosenEmployee;
+	int chosenEmployee, option;
 	//adding database later
 	Employee *hardCodedEmp[4];
 	hardCodedEmp[0] = new Employee("Markus Svensson", "45Fk2%93", "Male", "1976:05:02", "CEO", "HR", 15000);
@@ -27,16 +27,29 @@ int main() {
 	hardCodedEmp[2] = new Employee("Markus Svensson", "Ks01!2lR", "Male", "1983:02:08", "SPV", "PRO", 3200);
 	hardCodedEmp[3] = new Employee("Linda Skoge", "k#54uDl?", "Female", "1976:04:05", "GM", "HR", 7000);
 
-	for(int i=0; i <4; i++)
-	testEmployees.push_back(hardCodedEmp[i]);
+	for (int i = 0; i < 4; i++)
+		testEmployees.push_back(hardCodedEmp[i]);
 
 	srand(time(0));
 
 	choice = Menu();
 	switch (choice) {
 	case 1: {
-			chosenEmployee=Search(testEmployees, hardCodedIndex);
-			cout << testEmployees.at(chosenEmployee)->getName() << "<- returned from function";
+		char searchAgain = 'Y';
+		while (searchAgain == 'Y' || searchAgain == 'y') {
+			chosenEmployee = Search(testEmployees, hardCodedIndex, option);
+
+			try {
+				cout << testEmployees.at(chosenEmployee)->getName() << "<- returned from function" << endl;//testing
+				cout << endl << option << " <- option chosen" << endl;
+			}
+			catch (...) {
+				cout << "No employees found." << endl;
+			}
+			cin.clear();
+			cout << "Search again (Y/n)?";
+			cin >> searchAgain;
+		}
 		break;
 	}
 	case 2: {
@@ -59,7 +72,7 @@ int main() {
 
 	default: {
 		cout << "Enter a valid option";
-			break;
+		break;
 	}
 	}
 	cin.get();
@@ -105,7 +118,7 @@ string IDGenerator() {
 	return ID;
 }
 int ProfileMenu() {
-	system("CLS"); //will try to move all this from the function and only pass the value 
+	system("CLS");
 	int choice;
 	cout << "(1) View profile info" << endl;
 	cout << "(2) Update employee profile" << endl;
@@ -115,90 +128,85 @@ int ProfileMenu() {
 	cin >> choice;
 	return choice;
 }
-int Search(vector <Employee*> listOfEmployees, int index) { 
+int Search(vector <Employee*> listOfEmployees, int index, int& option) {
 	string searchWord;
 	vector <Employee*> foundEmployees; //vector to store all found employees
 	int found = 0; //counter	
-	vector <int> indexPosition; //store index from list or database 
-	char searchAgain = 'Y'; 
-	int choice, mod;
+	vector <int> indexPosition; //store index from list (database) 
+	int choice;
 
-	choice = ProfileMenu();
-	//thinking of moving this while-loop so question gets asked before break; in case 1.
+	option = ProfileMenu(); 
 
-	while (searchAgain == 'Y' || searchAgain == 'y') {//mby you should be able to search by only first name or surname. later fix 
-		cout << "Enter ID, name (forename surname), designation or department: ";
-		cin.ignore();
-		getline(cin, searchWord);
-		for (int i = 0; i < index; i++) {
-			if ((listOfEmployees.at(i)->getID()==searchWord) || (listOfEmployees.at(i)->getDesignation()==searchWord) || 
-				(listOfEmployees.at(i)->getDepartment()==searchWord) || (listOfEmployees.at(i)->getName() == searchWord)){
-				foundEmployees.push_back(listOfEmployees.at(i)); //push back found employees 
-				indexPosition.push_back(i);
-				found++; 
-			}
+	//mby you should be able to search by only first name or surname. later fix 
+	cout << "Enter ID, name (forename surname), designation or department: ";
+	cin.ignore();
+	getline(cin, searchWord);
+	
+	for (int i = 0; i < index; i++) {
+		if ((listOfEmployees.at(i)->getID() == searchWord) || (listOfEmployees.at(i)->getDesignation() == searchWord) ||
+			(listOfEmployees.at(i)->getDepartment() == searchWord) || (listOfEmployees.at(i)->getName() == searchWord)) {
+			foundEmployees.push_back(listOfEmployees.at(i)); //push back found employees 
+			indexPosition.push_back(i);
+			found++;
 		}
-		
-		if (found > 1) {
-			cout << found << " employees found: " << endl << endl;
+	}
 
-			if (choice == 1) {
-				cout << "Choose which profile to preview ";
-				for (int i = 0; i < found; i++) {
-					cout << endl << foundEmployees.at(i)->getName() << "(" << i + 1 << ") ";
-				}
-				cin >> mod;
-				return indexPosition.at(mod - 1);
-			}
+	if (found > 1) {
+		cout << found << " employees found: " << endl << endl;
 
-			if (choice == 2){
-				cout << "Choose which profile to modify ";
-				for (int i = 0; i < found; i++) {
-					cout << endl << foundEmployees.at(i)->getName() << "(" << i + 1 << ") ";
-				}
-				cin >> mod;
-				return indexPosition.at(mod - 1);
+		if (option == 1) {
+			cout << "Choose which profile to preview ";
+			for (int i = 0; i < found; i++) {
+				cout << endl << foundEmployees.at(i)->getName() << "(" << i + 1 << ") ";
 			}
-			else if (choice == 3) {
-				cout << "Choose which profile to delete ";
-				for (int i = 0; i < found; i++) {
-					cout << endl << foundEmployees.at(i)->getName() << "(" << i + 1 << ") ";
-				}
-				cin >> mod;
-				return indexPosition.at(mod - 1);
-			}
-			else if (choice == 4) {
-				cout << "Choose employee to promote: ";
-				for (int i = 0; i < found; i++) {
-					cout << endl << foundEmployees.at(i)->getName() << "(" << i + 1 << ") ";
-				}
-				cin >> mod;
-				return indexPosition.at(mod - 1);
-			}
-			else if (choice == 5) {
-				cout << "Choose employee for salary calculation: ";
-				for (int i = 0; i < found; i++) {
-					cout << endl << foundEmployees.at(i)->getName() << "(" << i + 1 << ") ";
-				}
-				cin >> mod;
-				return indexPosition.at(mod - 1);
-			}
+			cin >> choice;
+			return indexPosition.at(choice - 1);
 		}
 
-		else if (found == 0) {
-			cout << "No employees found.";
-			return 0;
+		if (option == 2) {
+			cout << "Choose which profile to modify ";
+			for (int i = 0; i < found; i++) {
+				cout << endl << foundEmployees.at(i)->getName() << "(" << i + 1 << ") ";
+			}
+			cin >> choice;
+			return indexPosition.at(choice - 1);
 		}
+		else if (option == 3) {
+			cout << "Choose which profile to delete ";
+			for (int i = 0; i < found; i++) {
+				cout << endl << foundEmployees.at(i)->getName() << "(" << i + 1 << ") ";
+			}
+			cin >> choice;
+			return indexPosition.at(choice - 1);
+		}
+		else if (option == 4) {
+			cout << "Choose employee to promote: ";
+			for (int i = 0; i < found; i++) {
+				cout << endl << foundEmployees.at(i)->getName() << "(" << i + 1 << ") ";
+			}
+			cin >> choice;
+			return indexPosition.at(choice - 1);
+		}
+		else if (option == 5) {
+			cout << "Choose employee for salary calculation: ";
+			for (int i = 0; i < found; i++) {
+				cout << endl << foundEmployees.at(i)->getName() << "(" << i + 1 << ") ";
+			}
+			cin >> choice;
+			return indexPosition.at(choice - 1);
+		}
+	}
 
-		else {
-			return indexPosition.front();
-		}
-		cin.clear();
-		cout << "Search again (Y/n)?";
-		cin >> searchAgain;
+	else if (found == 0) {
+		return -1; //if nothing was found
+	}
+
+	else { //if just one match is found
+		return indexPosition.front(); //send the only element in the vector
 	}
 }
 
-void ModifyProfile(vector <Employee*> listOfEmployees, int index, vector <int> indexPosition) {
 
+void ModifyProfile(vector <Employee*> listOfEmployees, int index, vector <int> indexPosition) {
+	
 }
